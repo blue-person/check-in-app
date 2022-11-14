8bb4daff5f0675a6634c298c1cdc401995c167ac
Carlospich <carlospich@unisabana.edu.co>
2022-11-13 20:11:00 -0500

    Update build.gradle

--- app/src/test/java/com/example/check/ExampleUnitTest.java
+++ app/src/test/java/com/example/check/ExampleUnitTest.java
@@ -1,17 +1,28 @@
 package com.example.check;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+@ExtendWith(MockitoExtension.class)
+
 public class ExampleUnitTest {
+
+    @InjectMocks
+    BookDao dao;
+
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    void Given_not_exists_When_call_register_book_Then_register_book() {
+        assertEquals(false,);
     }
 }
